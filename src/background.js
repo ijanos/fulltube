f90f70b95cbb21e9567e26cb7095468a7329c710
János Illés <ijanos@gmail.com>
2015-08-12 13:14:29 +0200

    use more robust urls and enable autoplay of the embeded video

--- src/background.js
+++ src/background.js
@@ -13,11 +13,12 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
-
 chrome.pageAction.onClicked.addListener(function(tab) {
   if (tab.url.indexOf("embed") > -1 ) {
-    chrome.tabs.update(tab.id, {url: tab.url.replace("embed/","watch?v=")});
-  } else {
-    chrome.tabs.update(tab.id, {url: tab.url.replace("watch?v=","embed/")});
+    var video_id = tab.url .split("embed/")[1].split("?")[0];
+    chrome.tabs.update(tab.id, {url: "https://youtube.com/watch?v=" + video_id});
+  } else if (tab.url.indexOf("watch") > -1 ) {
+    var video_id = tab.url .split("v=")[1].split("&")[0];
+    chrome.tabs.update(tab.id, {url: "https://youtube.com/embed/" + video_id + "?autoplay=1"});
   }
 });
